63b934e0a2c4a637214a3c3c8a3d521a54b6d4e7
mohan_palleti <palletimohanbabu23@gmail.com>
2022-01-03 17:19:14 +0530

    evaluation

---
+++ sprint-2/evaluation/index.js
@@ -0,0 +1,11 @@
+function get(n){
+    if(n==="l"){
+        window.location.href="www.google.com"
+    }
+    if(n==="s"){
+        window.location.href="www.google.com"
+    }
+    if(n==="m"){
+        window.location.href="menu.html"
+    }
+}
---
+++ sprint-2/evaluation/menu.js
@@ -0,0 +1,49 @@
+let url='https://www.themealdb.com/api/json/v1/1/categories.php';
+let data;
+var arr= JSON.parse(localStorage.getItem("foodcart")) || [];
+let count=document.querySelector("#cart_items");
+let x=arr.length;
+console.log(x)
+// count.textContent=x
+
+
+async function getdata(){
+    try{
+        let res= await fetch(url)
+         data= await res.json();
+        console.log(data);
+        displaydata(data); 
+
+    }
+    catch(err){
+        console.log(err)
+
+    }
+}
+getdata();
+
+function get(){
+window.location.href="cart.html";
+}
+function displaydata(data){
+    data.categories.forEach((element,index) => {
+        let div=document.createElement("div");
+        let button = document.createElement("button");
+        button.addEventListener("click",function(){
+            gotocart(index);
+        })
+        element.price=(Math.floor(Math.random(1)*500));
+        button.innerHTML="Add To Cart";
+        let p=document.createElement("p")
+        p.innerHTML=`Price: ${element.price}`;
+        let img=document.createElement("img");
+        img.src=element.strCategoryThumb;
+        div.append(img,p,button);
+        document.querySelector(".body").append(div);
+    });
+}
+function gotocart(index){
+ arr.push(data.categories[index]);
+ document.getElementById("cart_items").innerHTML=arr.length;
+ localStorage.setItem("foodcart",JSON.stringify(arr));
+}
